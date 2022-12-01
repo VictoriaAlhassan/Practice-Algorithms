7d905bede4c24f57258532968429fc478bccacdf
Victoria <victoriasoteria@gmail.com>
2022-12-01 06:26:14 -0800

    adds files on an algorithm

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
 // Reverse text without reversing individual words
 
-let str = 'The world is';
+let str = 'Technical Interview Preparation';
+let polishedString = str.split(" ");
+let reversedPolished = polishedString.reverse().join(' ')
 
-const reverseString = (str) => {
-    let newStr = [];
-    let string = "";
-    let reverseStr = "";
-    for (i = 0; i < str.length; i++) {
-       if (str[i] == " ") {
-           newStr.push(string);
-           string = "";
-       } else {
-           string += str[i];
-       }
-     }
-    if (string) {  
-    newStr.push(string);
-    } 
-    for (i = newStr.length - 1; i >= 0; i--) {
-         reverseStr += newStr[i] + " ";
-    }
-    return reverseStr;
-    };
   console.log(`Original string: ${str}`);
-  console.log( `Reversed string: ${reverseString(str)}`);
+  console.log( `Reversed string: ${reversedPolished}`);
 
 //Given a string and pattern containing wildcard character i.e *. 
 // Match to any number of characters to check if the pattern
@@ -50,7 +32,7 @@ function isMatch(str,p) {
 }
  str = "xyxzzxy";
  p = "x***x";
-if(isMatch(str, p) === true){
+if(isMatch(str, p)){
     console.log("String matches Pattern.");
 }else{
     console.log("String does not match pattern.");
